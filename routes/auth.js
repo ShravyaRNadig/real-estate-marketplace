17bf0e7cb591123da3b8392b1e07abdfbee846d5
k15460 <k15460@ktkbank.com>
2024-08-08 12:37:13 +0530

    require signin middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import * as auth from '../controllers/auth.js'
+import * as auth from '../controllers/auth.js';
+import { requireSignin } from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.get('/', auth.api);
+router.get('/', requireSignin, auth.api);
 router.post("/login", auth.login);
-router.post('/forgot-password',auth.forgotPassword)
+router.post('/forgot-password', auth.forgotPassword);
 
 export default router;
