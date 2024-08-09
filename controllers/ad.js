a1c22835fb3160a94123bd991ef96fd544838483
k15460 <k15460@ktkbank.com>
2024-08-09 13:08:57 +0530

    Save Ads

--- controllers/ad.js
+++ controllers/ad.js
@@ -1,5 +1,7 @@
 import { uploadImageToS3, deleteImageFromS3 } from '../helpers/upload.js';
 import { geocoderAddress } from '../helpers/google.js';
+import Ad from "../models/ad.js";
+import User from "../models/user.js";
 
 export const uploadImage = async (req, res) => {
     try {
@@ -49,14 +51,64 @@ export const removeImage = async (req, res) => {
 
 export const createAd = async (req, res) => {
     try {
-        const { address } = req.body;
+        const {
+            photos,
+            description,
+            address,
+            propertyType,
+            price,
+            landSize,
+            landsizetype,
+            action
+        } = req.body;
 
-        if (!address.trim()) {
-            return res.json({ error: "Address is required" });
+        const isRequired = (v) => {  // helper function to send error message
+            res.json({ error: `${v} is required` });
+        };
+
+        if (!photos || photos.length === 0) return isRequired("Photos");
+        if (!description) return isRequired("Description");
+        if (!address.trim()) return isRequired("Address");
+        if (!propertyType) return isRequired("Property Type");
+        if (!price) return isRequired("Price");
+        if (!action) return isRequired("Action");
+
+        if (propertyType === "Land") {
+            if (!landSize) return isRequired("Landsize");
+            if (!landsizetype) return isRequired("Landsize Type");
         }
 
         const { location, googleMap } = await geocoderAddress(address);
-        res.json({ location, googleMap });
+
+        try {
+
+            const ad = new Ad({
+                ...req.body,
+                slug: `${propertyType}-for-${action}-address-${address}-price-${price}-${nanoid(
+                    6
+                )}`,
+                postedBy: req.user._id,
+                location: {
+                    type: 'Point',
+                    coordinates: [location.coordinates[0], location.coordinates[1]]
+                },
+                googleMap,
+            });
+            await ad.save();
+
+            // update user role to seller
+            const user = await User.findByIdAndUpdate(req.user._id, {
+                $addToSet: { role: "Seller" },
+            });
+
+            // res.json({ok:true});
+            res.json({ ad, user });
+        } catch (err) {
+            console.log(err);
+            res.json({
+                error: "Create ad failed"
+            });
+        }
     } catch (err) {
         console.log(err);
         res.json({
