d37356bd791e4227206f5838431b7c1c3316edae
k15460 <k15460@ktkbank.com>
2024-08-13 15:19:49 +0530

    Pre depolyment optimization

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import authRoutes from './routes/auth.js';
 import adRoutes from "./routes/ad.js";
-import dotenv from 'dotenv';
+import helmet from "helmet";
+import compression from "compression";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
@@ -13,18 +15,34 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP 100 request per windowMs
+    message: "Too many request from this IP, please try again after 15 minutes",
+});
+app.use(limiter);
 
 // connect to mongodb
-mongoose.connect(process.env.DATABASE)
+mongoose
+    .connect(process.env.DATABASE)
     .then(() => {
         console.log("DB Connected");
+
         // routes middleware
         app.use('/', authRoutes);
         app.use('/', adRoutes);
+
+        // global error handler middleware
+        app.use((err, req, res, next) => {
+            console.error(err.stack);
+            res.status(500).send("Something went wrong!");
+        });
 
         app.listen(8000, () => {
             console.log("Server is running in 8000")
         });
     })
     .catch((err) => console.log("DB Connection Error =>", err));
-    dotenv.config();
